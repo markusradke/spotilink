9cba68c91eb72e7be79f2d80dd13fa31df8f9176
mradke <mail@markus-radke.de>
2024-01-11 18:48:59 +0100

    add fork spotifyr dependency

--- R/utils.R
+++ R/utils.R
@@ -3,6 +3,6 @@ rename_existing_variables <- function(input, database) {
   if(database == 'musicbrainz'){databaseVars <- musicbrainzVars}
   alreadyExistingCols <- colnames(input)[colnames(input) %in% databaseVars]
   new_names <- paste0(alreadyExistingCols, '_old')
-  cat(paste0('Renamed existing column for consistency (appended "_old"): ', alreadyExistingCols, '\n'))
+  cat(paste0('Renamed existing column for consistency (appended "_old"): ', alreadyExistingCols,'\n'))
   input %>% dplyr::rename_at(dplyr::vars(alreadyExistingCols), ~new_names)
 }
