bb992d0799ee87794bb6ee881b923a16844eace2
mradke <mail@markus-radke.de>
2024-10-09 10:53:57 +0200

    implement checkpoints for musicbrainz retrieval

--- R/get_artists_musicbrainz.R
+++ R/get_artists_musicbrainz.R
@@ -46,25 +46,40 @@ pull_artists_musicbrainz <- function(input, artist_threshold) {
 
   artists <- dplyr::select(artists, 'artist.s.id', 'artist.s.name', 'track.mb.artistlist','track.mb.quality')
 
-  artist_mbids <- purrr::pmap_df(artists, get_artist_MBIDs,
-                                 .progress = 'Searching artists on Musicbrainz...')
+  artist_mbids <- search_artist_mbids(artists)
+  result <- lookup_artists_mb(artist_mbids)
 
-  result <- purrr::pmap_df(artist_mbids, lookup_artists,
-                           .progress = 'Looking up artist information on Muiscbrainz...')
-
-  result <- dplyr::left_join(input, result, by = c('artist.s.id'))
 
   n_found_by_id <- result %>% dplyr::filter(artist.mb.foundbyid) %>% nrow()
   message(paste0(round(n_found_by_id / nrow(artists) * 100, 2), '% distinct artists were found using the Musicbrainz ID from the track information.'))
 
-  result %>%
-    filter_quality_musicbrainz_artists(artist_threshold) %>%
+  result <- result %>% filter_quality_musicbrainz_artists(artist_threshold) %>%
     dplyr::mutate(artist.mb.birth = as.Date(.data[['artist.mb.birth']]))  %>% # Date conversion must be here; else only integers are returned
     dplyr::mutate(artist.mb.death = as.Date(.data[['artist.mb.death']]))
+  result <- dplyr::left_join(input, result, by = c('artist.s.id'))
+
+  saveRDS(result, 'mb_artists.rds')
+  mbartist_remove_checkpoints()
+  result
 }
 
 
-get_artist_MBIDs <- function(artist.s.id, artist.s.name, track.mb.artistlist, track.mb.quality) {
+search_artist_mbids <- function(artists){
+  checkpoint_name <- 'mb_artists_search'
+  checkpoint <- read_checkpoint(checkpoint_name)
+  last_index <- checkpoint$last_index
+  if(! last_index == nrow(artists)){
+    saved_data <- checkpoint$saved_data
+    if(last_index > 0) {artists <- tail(artists, -last_index)}
+    purrr::pmap_df(artists,
+                   search_single_artist_mbid %>% save_checkpoint_and_count(checkpoint_name, last_index, saved_data),
+                   .progress = 'Searching artists on Musicbrainz...')
+  }
+  else{message('Detected checkpoint. Artist search already done.')}
+  suppressMessages(read_checkpoint(checkpoint_name)$saved_data)
+}
+
+search_single_artist_mbid <- function(artist.s.id, artist.s.name, track.mb.artistlist, track.mb.quality) {
   if(!is.na(track.mb.quality)) {
     artist<- track.mb.artistlist %>%
       dplyr::select('artist_mbid', 'name') %>%
@@ -100,7 +115,22 @@ get_artist_MBIDs <- function(artist.s.id, artist.s.name, track.mb.artistlist, tr
 }
 
 
-lookup_artists <- function(artist.s.id, artist.mb.id, artist.mb.quality, artist.mb.foundbyid) {
+lookup_artists_mb <- function(artist_mbids){
+  checkpoint_name <- 'mb_artists_lookup'
+  checkpoint <- read_checkpoint(checkpoint_name)
+  last_index <- checkpoint$last_index
+  if(! last_index == nrow(artist_mbids)){
+    saved_data <- checkpoint$saved_data
+    if(last_index > 0) {artist_mbids <- tail(artist_mbids, -last_index)}
+    purrr::pmap_df(artist_mbids,
+                   lookup_single_artist_mb %>% save_checkpoint_and_count(checkpoint_name, last_index, saved_data),
+                   .progress = 'Looking up artist information on Muiscbrainz...')
+  }
+  else{message('Detected checkpoint. Artist Lookup already done.')}
+  suppressMessages(read_checkpoint(checkpoint_name)$saved_data)
+}
+
+lookup_single_artist_mb <- function(artist.s.id, artist.mb.id, artist.mb.quality, artist.mb.foundbyid) {
   if(is.na(artist.mb.id)){return(make_na_frame_musicbrainz_artists(artist.s.id = artist.s.id))}
 
   musicbrainz::lookup_artist_by_id(artist.mb.id, includes = c('tags')) %>%
@@ -136,4 +166,12 @@ retrieve_artist_genre <- function(artists){
   cbind(artists, artistgenres) %>%
     dplyr::select(-'tags') %>%
     dplyr::mutate(artist.mb.topgenre = .data[['artist.mb.topgenre']] %>% as.character())
+}
+
+
+mbartist_remove_checkpoints <- function(){
+  pattern <- paste0('^mb_artists_(search|lookup)_(\\d+)\\.rds$')
+  files <- list.files()
+  matching_files <- grep(pattern, files, value = TRUE)
+  for(file in matching_files){file.remove(file)}
 }
